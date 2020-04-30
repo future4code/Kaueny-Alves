bc2a0e824f21bcfd79a8467caf975a777348ef44
Kaueny Alves <63555634+Kaueny-Alves@users.noreply.github.com>
2020-04-30 18:39:44 -0300

    Blackjack finalizado

--- semana3/blackjack/nossoJogo.js
+++ semana3/blackjack/nossoJogo.js
@@ -10,40 +10,34 @@
  * 
  * 
  */
-
-
 console.log ( "Bem vindo ao jogo de Blackjack!")
 
 
 if(confirm ("Quer iniciar uma nova rodada?"))  {
-   
-let usuario = []   
-let computador = []
 
+            const cart1 = comprarCarta()
+            const cart2 = comprarCarta()
+            const cart3 = comprarCarta()
+            const cart4 = comprarCarta()
 
+            let usuarioCarta = cart1.texto +" "+ cart3.texto
+            let usuariovalor = cart1.valor + " "+ cart3.valor
 
+            let computadorCarta = cart2.texto + cart4.texto
+            let computadorvalor = cart2.valor + cart4.valor
 
-const carta = comprarCarta()
-console.log (carta.texto)
-console.log (carta.valor)
+            console.log("Usuário - cartas: " + usuarioCarta + " - pontuação " + usuariovalor )
+            console.log("Computador - cartas: " + computadorCarta + " - pontuação " + computadorvalor )
 
-for (let i=0 ; i <= 1; i++ ){
-   computador.push(comprarCarta())
-   usuario.push(comprarCarta())  
+            if (usuariovalor > computadorvalor){
+               console.log("Usuario ganhou ");
+            }else if (usuariovalor < computadorvalor){
+               console.log("Computador ganhou ");
 
-}
+            }else {
+               console.log("Empate");
+            }
 
-console.log (usuario)
-console.log (computador[0][1])
-
-
-//console.log ("Usuário - cartas: " + usuario.values())
-console.log ("Usuario - cartas: " + usuario[0][1])
-console.log ("Computador -cartas: " + computador[1])
-
-
-
-
-}else {
-    console.log ("O jogo acabou.")
-}
+   }else {
+      console.log ("O jogo acabou.")
+  }
