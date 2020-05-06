d5b50152b1931eba046165206503c3c271ea6982
Kaueny Alves <63555634+Kaueny-Alves@users.noreply.github.com>
2020-05-06 16:35:36 -0300

    Objetos

---
+++ semana4/objetos/blog/index.js
@@ -0,0 +1,10 @@
+const blog = {
+    titulo:"Meu Primeiro Blog",
+    autor:"Kaueny Alves",
+    texto:"Hoje amanhaceu Lindo",
+}
+console.log(blog)
+
+let nomeDoAutor = blog.autor
+let nomeDoTitulo = blog.titulo
+let nomeDoTexto = blog.texto
