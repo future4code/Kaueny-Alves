d8f873219af2417859c91c0d72a522df43c49599
Kaueny Alves <63555634+Kaueny-Alves@users.noreply.github.com>
2020-05-07 09:10:44 -0300

    OBJ

--- semana4/objetos/blog/index.js
+++ semana4/objetos/blog/index.js
@@ -1,10 +1,50 @@
 const blog = {
-    titulo:"Meu Primeiro Blog",
-    autor:"Kaueny Alves",
-    texto:"Hoje amanhaceu Lindo",
+    titulo:[],
+    autor:[],
+    texto:[],
 }
+
+conteudo = []
+let contar = 0
+
 console.log(blog)
 
-let nomeDoAutor = blog.autor
-let nomeDoTitulo = blog.titulo
-let nomeDoTexto = blog.texto
+function capturarTexto (){
+
+  let recebeTitulo = document.getElementById ("titulo").value  
+  blog.titulo = recebeTitulo
+
+  let recebeTexto  = document.getElementById ("texto").value  
+  blog.texto = recebeTexto
+
+  let recebeAutor = document.getElementById ("autor").value  
+  blog.autor = recebeAutor
+ 
+ conteudo.push(blog)
+ console.log (blog)
+
+ enviarPosts()
+
+}
+
+function enviarPosts () {
+
+    let enviarTitulo = conteudo[contar].titulo
+    let enviarAutor = conteudo[contar].autor
+    let enviarTexto = conteudo[contar].texto
+
+    enviarPostsHtml = document.getElementById ("posts")
+    enviarPostsHtml.innerHTML += "<article>" + enviarTitulo + "</article>"
+
+    enviarPostsHtml = document.getElementById ("posts")
+    enviarPostsHtml.innerHTML += "<article>" + enviarAutor + "</article>"
+
+    enviarPostsHtml = document.getElementById ("posts")
+    enviarPostsHtml.innerHTML += "<article>" + enviarTexto + "</article>"
+
+    contar = contar +1
+}
+
+
+
+
