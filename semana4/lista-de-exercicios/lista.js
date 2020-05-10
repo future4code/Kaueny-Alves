e5826f2a5d19259b9ea6a3f736991e6211ff3416
Kaueny Alves <63555634+Kaueny-Alves@users.noreply.github.com>
2020-05-10 17:06:24 -0300

    Exercicios de interpretação de código

---
+++ semana4/lista-de-exercicios/lista.js
@@ -0,0 +1,36 @@
+//Exercicios de Interpretação de código
+
+//Analise os trechos de código em cada exercício e escreva o que ele faz, como faz e qual será o valor impresso no `console`.
+
+//exercicio 1
+//Essa função converte o valor em dólar para real.
+//O usuário informa o valor da cotação do dolar, 
+//depois esse valor é multiplicado por 100 (valor usado como parametro), para obter o resultado em Reais, 
+//o valor impresso é o valor do dolar convertido em Reais
+
+//exercicio 2
+//Essa função é para investir valores em diferentes tipos de investimentos.
+//Quando é informado o tipo de investimento e o valor essa função executa casos 
+//especificos para fazer o calculo investido e retornar o valor após o investimento.
+//o valor impresso, é o valor após investimento.
+//(novoMontante) Ações 165 - 
+//(segundoMontante) Tipo de investimento informado incorreto!
+
+//exercicio 3
+//Essa função é para verificar qual numero é par e qual é impar.
+//caso for par ele coloca no array1, caso for impar ele coloca no array2.
+// e imprime no console a quantidade de numeros em cada array.
+//numeros - Quantidade total de numeros: 14
+//array1 - 6
+//array2 - 8
+
+
+//exercicio 4
+//Essa função esta verificando qual numero é o menor e qual numero é o maior desse array.
+//numero1 recebe o menor numero
+//numero2 recebe o maior numero
+// o valor impresso é : 
+// -10  
+//  1590
+
+
