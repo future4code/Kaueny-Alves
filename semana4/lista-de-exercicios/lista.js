febbf3b7d37dcb78a89e5fd41c6adeb23959bc56
Kaueny Alves <63555634+Kaueny-Alves@users.noreply.github.com>
2020-05-10 23:32:26 -0300

    exercicios de funcoes de array

--- semana4/lista-de-exercicios/lista.js
+++ semana4/lista-de-exercicios/lista.js
@@ -278,3 +278,102 @@ function anonimarPessoa (){
 }
 anonimarPessoa()
 */
+
+
+
+//Exercícios de Funções de array
+
+//Exercício 1
+//a) Faça uma função que retorne um **novo array** só com os adultos 
+//(pessoas com idade igual ou superior a 20)
+/*
+let pessoas = [
+	{ nome: "Pedro", idade: 20 },
+	{ nome: "João", idade: 10 },
+	{ nome: "Paula", idade: 12 },
+	{ nome: "Artur", idade: 89 } 
+]
+
+let pessoasAdultas = pessoas.filter(function(item){
+
+    if(item.idade >= 20)
+    return true
+
+})
+    
+console.log (pessoasAdultas)
+*/
+//b) Faça uma função que retorne um **novo array** só com as crianças/adolescentes 
+//(pessoas com idade inferior a 20)
+/*
+let pessoas = [
+	{ nome: "Pedro", idade: 20 },
+	{ nome: "João", idade: 10 },
+	{ nome: "Paula", idade: 12 },
+	{ nome: "Artur", idade: 89 } 
+]
+
+let adolescentes = pessoas.filter(function(item){
+
+    if(item.idade < 20)
+    return true
+
+})
+console.log (adolescentes)
+*/
+
+
+//Exercício 2
+//Em todos os itens deste exercício, você terá que escrever uma **função** 
+//cuja entrada seja um **array**. Para testes, considere: `const array = [1, 2, 3, 4, 5, 6]`.
+   
+// a) Escreva uma função que **retorne** um array com as entradas multiplicadas por 2. 
+//Isto é `[2, 4, 6, 8, 10, 12]`.
+/*
+const array = [1, 2, 3, 4, 5, 6]
+let numerosPorDois = []
+
+numerosPorDois = array.map(function(item){
+     
+    return item *2
+
+ });
+
+ console.log(numerosPorDois)
+*/
+
+// b) Escreva uma função que **retorne** um array com as entradas multiplicadas 
+//por 3 e como strings. Isto é: `["3", "6", "9", "15", "18"]` 
+
+/*
+const array = [1, 2, 3, 4, 5, 6]
+let numerosPorTres = []
+
+
+numerosPorTres = array.map(function(item){
+     
+    return item *3
+
+ });
+
+ console.log(numerosPorTres.toString())
+*/
+
+// c) Escreva uma função que **retorne** um array de strings dizendo: 
+//"${número} é par/impar". Isto é: `["1 é impar", "2 é par", "3 é impar", 
+//"4 é par", "5 é impar", "6 é par"]`
+/*
+const array = [1, 2, 3, 4, 5, 6]
+let numerosParImpar = []
+
+numerosParImpar = array.map(function(item){
+     
+    if(item %2 === 0){
+     return `${item} é par`
+    } else {
+        return `${item} é impar`
+    }
+ });
+
+console.log (numerosParImpar)
+*/
