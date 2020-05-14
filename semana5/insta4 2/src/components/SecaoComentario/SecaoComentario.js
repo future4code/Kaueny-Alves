514bc188e6e9adb936ffd525841e221fe56b6eea
Kaueny Alves <63555634+Kaueny-Alves@users.noreply.github.com>
2020-05-14 15:38:46 -0300

    alterações

--- semana5/insta4 2/src/components/SecaoComentario/SecaoComentario.js
+++ semana5/insta4 2/src/components/SecaoComentario/SecaoComentario.js
@@ -3,25 +3,24 @@ import './SecaoComentario.css'
 
 export class SecaoComentario extends Component {
 	state = {
-		Comentario: "",
+		Comentario: " ",
 	}
 
-	onChangeComentario = (evento)=> {
-		const novoComentario = evento.target.value;
-		this.setState({Comentario: novoComentario})
-
-	}
-
-	render() {
-		return <div className={'comment-container'}>
-			<input
-				className={'input-comentario'}
-				placeholder={'Comentário'}
-				value={this.state.comentario}
-				onChange={this.onChangeComentario}
-			/>
-			<button onClick={this.props.aoEnviar}>Enviar</button>
-		</div>
-	}
-}
+	onChangeComentario = (event) => {
+		console.log(event.target.value)
+	  this.setState({valorInputComentario: event.target.value})
+	  }
+  
+	  render() {
+		  return <div className={'comment-container'}>
+			  <input
+				  className={'input-comentario'}
+				  placeholder={'Comentário'}
+				  value={this.state.valorInputComentario}
+				  onChange={this.onChangeComentario}
+			  />
+			  <button onClick={this.props.aoEnviar}>Enviar</button>
+		  </div>
+	  }
+  }
 
