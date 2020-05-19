11bcfee123693c06b6670b4fd8108ed6c50ffe78
Kaueny Alves <63555634+Kaueny-Alves@users.noreply.github.com>
2020-05-19 16:34:12 -0300

    função botão

--- semana6/labeform-app/src/App.js
+++ semana6/labeform-app/src/App.js
@@ -1,48 +1,58 @@
 import React from 'react';
-import styled from 'styled-components';
-import Etapa1 from './componentes/Etapa1';
-import Etapa2 from './componentes/Etapa2';
-import Etapa3 from './componentes/Etapa3';
-import Final from './componentes/Final';
+ import styled from 'styled-components';
+ import Etapa1 from './componentes/Etapa1';
+ import Etapa2 from './componentes/Etapa2';
+ import Etapa3 from './componentes/Etapa3';
+ import Final from './componentes/Final.js';
 
-class App extends React.Component {
 
-  state = {
-    etapa: 1,
-    
-    
-  }
+  const Container = styled.div `
+    display: block;
+    text-align: center;
+  `
 
-  renderizaEtapa = ()=>{
-    switch (this.state.etapa){
-      case 1:
-        return <Etapa1 />;
-      case 2:
-        return <Etapa2 />;
-      case 3:
-        return <Etapa3 />;
-      case 4:
-        return <Final />;
-      default:
-        return
-    }
-  }
+ class App extends React.Component {
 
-  irParaProximaEtapa () {
-    this.setState({etapa:2 })
 
-  }
   
-
-  render (){
+   state = {
+     etapa: 1
     
-  return (
-    <div>
-      {this.renderizaEtapa()}
-      <br/>
-      <button onClick={this.irParaProximaEtapa}>Próxima etapa</button>
-    </div>
-  );
-}
-}
-export default App;
+   }
+  
+   
+   renderizaEtapa = ()=>{
+     switch (this.state.etapa){
+       case 1:
+         return <Etapa1 />;
+       case 2:
+         return <Etapa2 />;
+       case 3:
+         return <Etapa3 />;
+       case 4:
+         return <Final />;
+       default:
+         return
+     }
+   }
+
+
+
+   irParaProximaEtapa = () => {
+     this.setState({etapa: this.state.etapa + 1})
+   }
+    
+
+   render (){
+
+   return (
+      
+     <Container>
+       {this.renderizaEtapa()}
+       <br/>
+       {this.state.etapa < 4 && <button onClick={this.irParaProximaEtapa}>Próxima etapa</button> }
+     </Container>
+   );
+ }
+ }
+ export default App;
