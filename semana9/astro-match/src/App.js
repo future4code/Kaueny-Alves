a838b237f42a0c0020b567e318e3bcd70563a664
Kaueny Alves <63555634+Kaueny-Alves@users.noreply.github.com>
2020-06-23 21:44:12 -0300

    funcoes axios  funcionando

--- semana9/astro-match/src/App.js
+++ semana9/astro-match/src/App.js
@@ -1,49 +1,26 @@
 import React, {useState} from 'react';
 import Card from '@material-ui/core/Card';
-import { makeStyles } from '@material-ui/core/styles';
-import Home from './Components/home/home';
-import Matches from './Components/matches/matches';
-import Clear from './Components/clear/clear';
-import PeopleIcon from '@material-ui/icons/People';
+import Main from './Components/Main/Main';
+import  ResetButton from './Components/ResetButton/ResetButton';
+import styled from 'styled-components';
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-    textAlign:"center",
-    display:"block",
-    margin:" auto",
-    marginTop: 50,
-  },
-  media:{
-    padding:30,
+const MainContainer = styled(Card)` 
+  border-radius: 3px;
+  width: 400px;
+  height: 600px;
+  margin: 50px;
+`;
 
-  }
-  
-});
 
 function App() {
-  const classes = useStyles()
- 
-  const [currentPage, setCurrentPage] = useState("Home")
-  
-  const changePage=()=>{
-    if(currentPage === "Home" ){
-    setCurrentPage("Mathes")
-  } else {
-    setCurrentPage("Home")
-  }
-}
-
   
 
   return (
     <>
-    <Card className={classes.root}>
-      <PeopleIcon onClick={changePage} >trocar de tela</PeopleIcon>
-      {currentPage === "Home" ?  <Home></Home> : <Matches></Matches> }
-     
-   </Card>
-   <Clear></Clear>
+    <MainContainer>
+      <Main/>
+   </MainContainer>
+   <ResetButton/>
    </>
   );
 }
