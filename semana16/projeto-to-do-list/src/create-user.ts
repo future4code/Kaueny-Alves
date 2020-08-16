caab08f29df77c441de2bde781926e8c4737d6e6
Kaueny de Oliveira Alves <kaueny@MacBook-Pro-de-Kaueny.local>
2020-08-16 19:00:50 -0300

    criação das tarefas

--- semana16/projeto-to-do-list/src/create-user.ts
+++ semana16/projeto-to-do-list/src/create-user.ts
@@ -1,7 +1,5 @@
 import knex from "knex";
-import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-import { AddressInfo } from "net";
 
 /**************************************************************/
 
